bd5652c884564b2fbadbef3466c1d0d86b737586
Bruno de Souza Ferreira Carneiro <bruno.carneiro@spread.com.br>
2018-05-14 17:48:28 -0300

    setup entidades

--- src/main/java/br/com/brb/ppe/service/impl/AcordoServiceImpl.java
+++ src/main/java/br/com/brb/ppe/service/impl/AcordoServiceImpl.java
@@ -1,24 +1,29 @@
 package br.com.brb.ppe.service.impl;
 
+import br.com.brb.ppe.domain.Pagamento;
+import br.com.brb.ppe.domain.dto.CadastrarResultadoPedidoDTO;
+import br.com.brb.ppe.repository.AcordoRepository;
 import br.com.brb.ppe.service.AcordoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class AcordoServiceImpl implements AcordoService {
-
+    
     @Override
-    public void cadastrarResultadoAnalisePedido() {
+    public void cadastrarResultadoAnalisePedido(CadastrarResultadoPedidoDTO analisePedido) {
 
     }
 
     @Override
-    public void informarPagamento() {
+    public void informarPagamento(Pagamento pagamento) {
 
     }
 
     @Override
-    public void obterRespostaProposta() {
+    public void obterRespostaProposta(Date dataInicio, Date dataFim, String guidPedido) {
 
     }
-
 }
--- src/main/java/br/com/brb/ppe/service/impl/DocumentoServiceImpl.java
+++ src/main/java/br/com/brb/ppe/service/impl/DocumentoServiceImpl.java
@@ -1,23 +1,26 @@
 package br.com.brb.ppe.service.impl;
 
+import br.com.brb.ppe.domain.dto.IdentificadorArquivoDTO;
 import br.com.brb.ppe.service.DocumentoService;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class DocumentoServiceImpl implements DocumentoService {
 
     @Override
-    public void confirmarRecebimentoArquivo() {
+    public void confirmarRecebimentoArquivo(Set<IdentificadorArquivoDTO> listaIdentificadorArquivos) {
 
     }
 
     @Override
-    public void obterArquivo() {
+    public void obterArquivo(IdentificadorArquivoDTO identificadorArquivoDTO) {
 
     }
 
     @Override
-    public void obterDocumento() {
+    public void obterDocumento(String guidPedido) {
 
     }
 }
